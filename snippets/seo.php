eb7754673b8bbf2cd469d36acae70c1190954a64
yahnodewit-libis <yahno.dewit@kuleuven.be>
2026-06-25 16:14:25 +0200

    fix in plugin

--- snippets/seo.php
+++ snippets/seo.php
@@ -6,12 +6,16 @@ $robots = $page->seoRobots();
 $canonical = $page->seoCanonical();
 
 $og = [
-  'title' => $page->og_title() != "" ? $page->og_title() : $page->seoTitle(),
-  'description' => $page->og_description() != "" ? $page->og_description() : $site->site_meta_description(),
-  'image' => $page->og_image()->toFile() != "" ? $page->og_image()->toFile()->url() : ($site->og_default_image()->toFile() != "" ? $site->og_default_image()->toFile()->url() : ""),
+  'title' => $page->og_title()->or($page->seoTitle())->value(),
+  'description' => $page->og_description()->or($site->site_meta_description())->value(),
+  'image' => $page->og_image()->toFile()
+      ? $page->og_image()->toFile()->url()
+      : ($site->og_default_image()->toFile()
+          ? $site->og_default_image()->toFile()->url()
+          : ''),
   'url' => $page->url(),
-  'site_name' => $site->og_site_name(),
-  'type' => "Website",
+  'site_name' => $site->og_site_name()->value(),
+  'type' => 'website',
 ];
 ?>
 
@@ -29,14 +33,14 @@ $og = [
   <link rel="canonical" href="<?= esc($canonical) ?>" />
   <?php endif ?>
 
-    <meta property="og:title" content="<?= esc($og['title']) ?>">
-    <meta property="og:description" content="<?= esc($og['description']) ?>">
-    <meta property="og:url" content="<?= esc($og['url']) ?>">
-    <meta property="og:site_name" content="<?= esc($og['site_name']) ?>">
-    <meta property="og:type" content="<?= esc($og['type']) ?>">
+    <meta property="og:title" content="<?= esc($og['title'] ?? '') ?>">
+    <meta property="og:description" content="<?= esc($og['description'] ?? '') ?>">
+    <meta property="og:url" content="<?= esc($og['url'] ?? '') ?>">
+    <meta property="og:site_name" content="<?= esc($og['site_name'] ?? '') ?>">
+    <meta property="og:type" content="<?= esc($og['type'] ?? '') ?>">
 
   <?php if ($og['image']): ?>
-  <meta property="og:image" content="<?= esc($og['image']) ?>">
+    <meta property="og:image" content="<?= esc($og['image'] ?? '') ?>">
   <?php endif ?>
 
     <meta name="twitter:card" content="summary_large_image">
